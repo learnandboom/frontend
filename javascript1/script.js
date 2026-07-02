919ee38cc4aae6ba9f36f20e376409c545687804
Aman Kumar <nazarandaazx@email.com>
2026-07-02 22:46:53 +0530

    add some files

---
+++ javascript1/script.js
@@ -0,0 +1,34 @@
+// // // console.log("hello world");
+// // {
+// //   var a = 22;
+// //   console.log(a);
+// //   var a = 11;
+// //   console.log(a);
+// // }
+// // console.log(a);
+// // {
+// //   let a = 22;
+// //   a = 1;
+// //   console.log(a);
+// // }
+// // console.log(a);
+// // let name = "aman";
+// // console.log(name, "name");
+// // let num = 1;
+// // console.log(num);
+// // let isIt = true;
+// // console.log(isIt);
+// // let null_ = null;
+// // console.log(null_);
+// //                                    operators
+// // let a = 2 ** 2; ////power
+// // console.log(a);
+// // let b = 8 % 3;
+// // console.log(b);
+// // let c = 20 / 2;
+// // console.log(c);
+// let y = 4;
+// let z = 2;
+// console.log(y++);
+// console.log(y);
+// console.log(++z);
